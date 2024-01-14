3813f9382be160355525ab6759555849574df7a3
Sukhaman Singh <sukhamandev@gmail.com>
2024-01-14 15:57:34 -0500

    framework added

---
+++ Sources/ViewServiceManager.xcframework/ios-arm64_x86_64-simulator/ViewServiceManager.framework/Headers/ViewServiceManager.h
@@ -0,0 +1,18 @@
+//
+//  ViewServiceManager.h
+//  ViewServiceManager
+//
+//  Created by Sukhaman Singh on 1/14/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ViewServiceManager.
+FOUNDATION_EXPORT double ViewServiceManagerVersionNumber;
+
+//! Project version string for ViewServiceManager.
+FOUNDATION_EXPORT const unsigned char ViewServiceManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewServiceManager/PublicHeader.h>
+
+
